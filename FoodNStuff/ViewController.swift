e2c7ab21e943b9123cb10168894f6d339a4e6fb5
thellamapaul <liamrbriggs@gmail.com>
2018-01-10 14:31:04 -0600

    Initial Commit

---
+++ FoodNStuff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodNStuff
+//
+//  Created by ITACD Student Laptop on 1/10/18.
+//  Copyright © 2018 Liam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
